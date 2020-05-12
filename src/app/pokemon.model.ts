b4907ebac204dcc1000351d59a774034f2cb43a4
brian-bourdon <brian78.b@gmail.com>
2020-05-12 14:51:03 +0200

    Model pokemon

---
+++ src/app/pokemon.model.ts
@@ -0,0 +1,18 @@
+export class Pokemon {
+    public name: string;
+    public hp: number;
+    public speed: number;
+    public attack: number;
+  
+    constructor(name: string, hp: number, speed: number, attack: number) {
+      this.name = name;
+      this.hp = hp;
+      this.speed = speed;
+      this.attack = attack;
+    }
+  
+    attackPokemon(receiver: Pokemon)
+    {
+      receiver.hp = receiver.hp - this.attack;
+    }
+}
