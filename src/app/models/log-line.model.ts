d684203ae9511044e02ce94a50cd41aa48c32889
ClementLegouest <clement.legouest@gmail.com>
2020-05-14 16:11:11 +0200

    Time in logs

--- src/app/models/log-line.model.ts
+++ src/app/models/log-line.model.ts
@@ -1,9 +1,11 @@
 export class LogLine {
   line: string;
   color: string;
+  date: Date;
 
-  constructor(line: string, color: string) {
+  constructor(line: string, color: string, date: Date) {
     this.line = line;
     this.color = color;
+    this.date = date;
   }
 }
