e3e5ab8587a76b4f0e109bd150424dfedf77be4d
ClementLegouest <clement.legouest@gmail.com>
2020-05-15 15:33:28 +0200

    selection page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BattleComponent } from "./battle/battle.component";
+import { SelectComponent } from "./select/select.component";
 
 
 const routes: Routes = [
   { path: 'battle', component: BattleComponent },
-  { path: '', component: BattleComponent },
+  { path: 'select', component: SelectComponent },
+  { path: '', component: SelectComponent },
   // { path: 'not-found', component: FourOhFourComponent },
   // { path: '**', redirectTo: 'not-found' }
 ];
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -2,25 +2,74 @@ import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Pokemon } from '../models/pokemon.model';
+import { map } from "rxjs/operators";
+
+interface RawPokemon {
+  name: string,
+  stats: Stat[],
+  sprites: Sprite
+}
+
+interface Stat {
+  base_stat: number,
+  stat: StatData
+}
+
+interface StatData {
+  name: string
+}
+
+interface Sprite {
+  front_default: string
+}
 
 @Injectable()
 export class PokemonService {
 
-    urlApi = 'https://pokeapi.co/api/v2/pokemon/';  // URL de l'API
+  urlApi = 'https://pokeapi.co/api/v2/pokemon/';  // URL de l'API
+  selectedPokemons: Pokemon[] = [];
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    getPokemonByNameFromApi(pokemon: Pokemon): Observable<Pokemon> {
+  getPokemonByNameFromApi(pokemon: Pokemon): Observable<Pokemon> {
+    return this.http.get<Pokemon>(this.urlApi + pokemon.name);
+  }
 
-        return this.http.get<Pokemon>(this.urlApi + pokemon.name);
+  getPokemonByIdFromApi(pokemonId: number): Observable<Pokemon> {
+    return this.http.get<RawPokemon>(this.urlApi + pokemonId)
+      .pipe(
+        map((rawPokemon: RawPokemon): Pokemon => {
+          console.log(rawPokemon);
+          return new Pokemon(
+            rawPokemon.name,
+            this.findStat(rawPokemon, 'hp').base_stat,
+            this.findStat(rawPokemon, 'attack').base_stat,
+            this.findStat(rawPokemon, 'speed').base_stat,
+            rawPokemon.sprites.front_default
+          );
+        })
+      );
+  }
 
+  findStat(rawPokemon: RawPokemon, statName: string) {
+    return rawPokemon.stats.find(stat => stat.stat.name === statName);
+  }
+
+  getSelectedPokemons(): Pokemon[] {
+    return this.selectedPokemons;
+  }
+
+  addOneToSelection(pokemon: Pokemon): void {
+    this.selectedPokemons.push(pokemon);
+  }
+
+  removeOneFromSelection(pokemon: Pokemon): void {
+    for( let i = 0; i < this.selectedPokemons.length; i++ ) {
+      if (pokemon.name === this.selectedPokemons[i].name ) {
+        this.selectedPokemons.slice(i, 1);
+        console.log('index : ', i);
+      }
     }
-
-    getPokemonByIdFromApi(pokemonId: number): Observable<Pokemon> {
-
-        return this.http.get<Pokemon>(this.urlApi + pokemonId );
-
-    }
-
+  }
 }
 
