4d39affddea22975dc0ea06b86357c946bcd23b6
Kyriel DOSSOU <Kyrigami@gmail.com>
2020-05-15 14:58:48 +0200

    test passed

--- src/app/services/game.spec.ts
+++ src/app/services/game.spec.ts
@@ -7,15 +7,11 @@ import { Pokemon } from '../models/pokemon.model';
 describe('Test game.ts', () => {
 
     test('Game winner', () => {
-        const ratata = new Pokemon('ratata', 200, 20, 20);
-        const pika = new Pokemon('pika', 0, 20, 20);
-        // rounds
-        const fight1 = new BattleService(ratata, pika);
-        const fight2 = new BattleService(ratata, pika);
-        const fight3 = new BattleService(ratata, pika);
+        const ratata = new Pokemon('ratata', 200, 20, 20, 'image');
+        const pika = new Pokemon('pika', 0, 20, 20, 'image');
         // game
-        const game = new Game(fight1, fight2, fight3);
-        expect(game.gameWinner()).toBe('ratata');
+        const game = new Game();
+        expect(game.gameWinner(ratata.name, pika.name, pika.name)).toBe('ratata');
 
     });
 
---
+++ src/app/services/pokemon.service.spec.ts
@@ -0,0 +1,12 @@
+import { PokemonService } from './pokemon.service';
+import { Pokemon } from '../models/pokemon.model';
+
+describe('get pokemon by Id', () => {
+  it('getPokemonById', () => {
+  });
+});
+
+describe('get pokemon by Name', () => {
+  it('getPokemonByName', () => {
+  });
+});
